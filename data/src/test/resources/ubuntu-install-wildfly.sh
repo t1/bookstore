40e39af4bdc99098bf6ce5708886521e4e3a1bf8
Rüdiger zu Dohna <snackbox@sinntr.eu>
2019-10-24 12:40:04 +0200

    document aws install experiments

---
+++ data/src/test/resources/ubuntu-install-wildfly.sh
@@ -0,0 +1,25 @@
+# install
+
+WILDFLY_VERSION=18.0.0.Final
+
+alias l='ls -lahF'
+sudo apt-get update
+sudo apt-get dist-upgrade
+sudo apt-get install httpie
+sudo apt-get install openjdk-11-jdk-headless
+http --download https://download.jboss.org/wildfly/${WILDFLY_VERSION}/wildfly-${WILDFLY_VERSION}.tar.gz
+tar xzf wildfly-${WILDFLY_VERSION}.tar.gz
+
+# start
+~/wildfly-${WILDFLY_VERSION}/bin/standalone.sh -b 0.0.0.0
+
+# config
+~/wildfly-${WILDFLY_VERSION}/bin/jboss-cli.sh --connect
+/subsystem=logging/console-handler=CONSOLE:write-attribute(name=level,value=ALL)
+
+# deploy
+AWS_SERVER_NAME=ec2-18-196-165-203.eu-central-1.compute.amazonaws.com
+scp -i /Users/rdohna/.ssh/aws.pem target/books.war ubuntu@${AWS_SERVER_NAME}:
+ssh -i /Users/rdohna/.ssh/aws.pem ubuntu@${AWS_SERVER_NAME}
+~/wildfly-${WILDFLY_VERSION}/bin/jboss-cli.sh --connect
+deploy --force books.war
